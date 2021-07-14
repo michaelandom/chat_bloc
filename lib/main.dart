881950369f308c33721f20db8e70eacc8122f4e1
michael andom <michaelandom03@gmail.com>
2021-07-14 10:59:23 +0300

    notification added

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,56 @@ import 'package:chat_bloc/bloc/chat/chat_bloc.dart';
 import 'package:chat_bloc/bloc/mybloc.dart';
 import 'package:chat_bloc/routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc/theme/theme_bloc.dart';
 
+Future<void> backgroundMessage(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+  print(message.notification!.body);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundMessage);
   Bloc.observer = MyBlocObserver();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   Routers routers = Routers();
+
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final routerFromMessage = message.data["route"];
+        print(routerFromMessage);
+      }
+    });
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification!.body);
+        print(message.notification!.title);
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routerFromMessage = message.data["route"];
+      print(routerFromMessage);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
